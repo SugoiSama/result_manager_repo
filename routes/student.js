7097a5454151f7832f226a0605b86cf7a217338d
Akash Shakya <akash.r.shakya99@gmail.com>
2025-01-21 10:22:07 +0545

    initial commit

--- routes/student.js
+++ routes/student.js
@@ -1,8 +1,8 @@
-import {Router} from 'express';
-import { getStudentList, createStudent } from '../controllers/student.js';
+import { Router } from "express";
+import { getStudentList, createStudent } from "../controllers/student.js";
 const studentRouter = new Router();
 
-studentRouter.get('/', getStudentList);
-studentRouter.get('/', createStudent);
+studentRouter.get("/", getStudentList);
+studentRouter.post("/", createStudent);
 
 export default studentRouter;
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ dotenv.config();
 import express from "express";
 import { connectDB } from "./config/db.js";
 import studentRouter from "./routes/student.js";
+import subjectRouter from "./routes/subject.js";
 
 const app = express();
 app.use(express.json());
 app.use("/api/student", studentRouter);
+app.use("/api/subject", subjectRouter);
 connectDB();
 
 app.listen(3000, () => {
